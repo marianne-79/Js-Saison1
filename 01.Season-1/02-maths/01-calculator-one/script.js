5329c0cbea43b1c14f8cda3075c5495cbe0da637
marianne-79 <84538997+marianne-79@users.noreply.github.com>
2021-06-21 17:13:53 +0200

    ajoute exo 2.1

--- 01.Season-1/02-maths/01-calculator-one/script.js
+++ 01.Season-1/02-maths/01-calculator-one/script.js
@@ -6,17 +6,33 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let a = new Number(document.getElementById("op-one").value);
+        let b = new Number(document.getElementById("op-two").value);
+        let total = a + b ;
+        alert(total);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let a = new Number(document.getElementById("op-one").value);
+        let b = new Number(document.getElementById("op-two").value);
+        let total = a - b ;
+        alert(total);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let a = new Number(document.getElementById("op-one").value);
+        let b = new Number(document.getElementById("op-two").value);
+        let total = a * b ;
+        alert(total);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let a = new Number(document.getElementById("op-one").value);
+        let b = new Number(document.getElementById("op-two").value);
+        let total = a / b ;
+        alert(total);
     });
 })();
